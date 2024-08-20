5030d703e4257589c873d33bef51a21e7e9092e0
Darius <excipial@gmail.com>
2024-08-20 19:50:01 +0300

    up

--- js/productDetails.js
+++ js/productDetails.js
@@ -1,22 +1,29 @@
 export function productDetails(list, id) {
-    // Suranda produktą pagal jo ID iš sąrašo
     const product = list.find(item => item.id === id);
 
-    // Jei produktas nerastas, grąžina pranešimą
     if (!product) {
         return `Prekė, su ID: ${id} neegzistuoja.`;
     }
 
-    // Jei produktas rastas, suformuoja išsamią produkto informaciją
-    return `
-------------------------------
+    const totalPrice = (product.amount * product.unitPrice / 100).toFixed(2);
+
+    const longestFieldLength = Math.max(
+        'Pavadinimas   | '.length + product.name.length,
+        'Kiekis        | '.length + product.amount.toString().length + 4, // " vnt" užima 4 simbolius
+        'Vieneto kaina | '.length + (product.unitPrice / 100).toFixed(2).length + 4, // " Eur" užima 4 simbolius
+        'Viso mokėti   | '.length + totalPrice.length + 4 // " Eur" užima 4 simbolius
+    );
+
+    const line = '-'.repeat(longestFieldLength);
+
+    return `${line}
 Prekės informacija
-------------------------------
+${line}
 ID            | ${product.id}
 Pavadinimas   | ${product.name}
 Kiekis        | ${product.amount} vnt
 Vieneto kaina | ${(product.unitPrice / 100).toFixed(2)} Eur
-Viso mokėti   | ${(product.amount * product.unitPrice / 100).toFixed(2)} Eur
-------------------------------
-    `;
+Viso mokėti   | ${totalPrice} Eur
+${line}`;
 }
+
